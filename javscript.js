fceaa3889f6fa2985493277ff85f004c7ebd2d3f
Kamil <108445618+crawford-kamil@users.noreply.github.com>
2024-02-18 23:11:57 -0500

    Finish functions for getting the computer's choice and playing a round.

--- javscript.js
+++ javscript.js
@@ -1,1 +1,36 @@
-prompt("Choose rock, paper, or scissors.", "")
+let playerChoice = (prompt("Choose rock, paper, or scissors.", ""));
+
+function getComputerChoice() {
+    let randomNumber = (Math.random() * 1);
+
+    return (randomNumber <= 33) ? (rock) 
+    : (randomNumber >= 66) ? (paper)
+    : (scissors) ;
+};
+
+
+function round() {
+    let rock = "rock";
+    let scissors = "scissors";
+    let paper = "paper";
+    let win = `You win. ${playerChoice} beats ${getComputerChoice()}.`;
+    let loss = `You lose. ${playerChoice} loses to ${getComputerChoice()}.`;
+    let tie = `It's a tie ${playerChoice} ties with ${getComputerChoice()}.`;
+
+    if (playerChoice.toLowerCase === "rock" && getComputerChoice() === scissors
+    || playerChoice.toLowerCase === "scissors" && getComputerChoice() === paper
+    || playerChoice.toLowerCase === "paper" && getComputerChoice() === rock) {
+        return win;
+    } else if (getComputerChoice() === rock && playerChoice.toLowerCase === "scissors"
+    || getComputerChoice() === scissors && playerChoice.toLowerCase === "paper"
+    || getComputerChoice() === paper && playerChoice.toLowerCase === "rock") {
+        return loss;
+    } else if (playerChoice.toLowerCase === "rock" && getComputerChoice() === rock
+    || playerChoice.toLowerCase === "scissors" && getComputerChoice() === scissors
+    || playerChoice.toLowerCase === "paper" && getComputerChoice() === paper) {
+        return tie;
+    } 
+}
+    game(); {
+
+    }
